4701178b1ec5546f8095c04e4e9b719a8a49a68c
Manoj Kumar <prajapatmanoj609@gmail.com>
2022-01-14 00:18:29 +0530

    first commit

---
+++ js/music-list.js
@@ -0,0 +1,122 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Ishq shufiyaana",
+    artist: "zee music",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "With light inside",
+    artist: "chen Xueran",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Should be me",
+    artist: "Nightcore",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "AAj din chadheya",
+    artist: "pritam",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Kabira",
+    artist: "T-series",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Kitni Haseen Zindgi",
+    artist: "Lucky-ali",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Yaar ki shaadi",
+    artist: "sumit goswami",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Mere yaar ",
+    artist: "karan benipal",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Daryaa",
+    artist: "Eros music",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Jee karda",
+    artist: "Gkhan/ khan saab",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "churai janda Eh",
+    artist: "jassi gill",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Baari",
+    artist: "Bilal saeed",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Dhun lagi",
+    artist: "Siddarth amit",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Rangi saari",
+    artist: "kavita seth",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "chidiyaa",
+    artist: "vilen",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Daru badnaam",
+    artist: "kamal kahlon",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Freedom",
+    artist: "K'NAAN",
+    img: "music-17",
+    src: "music-17"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
